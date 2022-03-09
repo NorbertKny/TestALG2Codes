f63fdd56f198f9776f9c3d61ccb89448f0974890
Norbert Roland Knýgit config --global user.email norbert.roland.kny@tul.czmkdir Codecd Codegit clone https://github.com/pepe/gitwstul22.gitgit config --global user.name Norbert Roland Knýgit config --global user.name <norbert.roland.kny@tul.cz>
2022-03-09 14:26:18 +0100

    new stuff

--- test2/src/test2/NewMain1.java
+++ test2/src/test2/NewMain1.java
@@ -8,14 +8,13 @@ package test2;
  *
  * @author norbert.roland.kny
  */
-public class NewMain {
+public class NewMain1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello world");
     }
     
 }
