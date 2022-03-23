5a2a008e63e1f0023f6a8ea30c38177377dfd49f
Norbert Roland Knýgit config --global user.email norbert.roland.kny@tul.czmkdir Codecd Codegit clone https://github.com/pepe/gitwstul22.gitgit config --global user.name Norbert Roland Knýgit config --global user.name <norbert.roland.kny@tul.cz>
2022-03-23 16:13:39 +0100

    test2

--- test2/src/test2/Test2.java
+++ test2/src/test2/Test2.java
@@ -5,6 +5,8 @@
  */
 package test2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author norbert.roland.kny
@@ -16,9 +18,56 @@ public class Test2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello world");
-        double pie = Math.PI;
-        String s = "";
+        //System.out.println("Hello world");
+        //double pie = Math.PI;
+        //String s = "";
         //very sus
+        //getDatum ==> datum ==> urci prestupnoat
+        int datum = 1600;
+        boolean prestupny = false;
+        if( (datum % 100 == 0) && (datum % 400 == 0) && (datum % 4 == 0) ){
+            prestupny = true;
+        }
+        System.out.println(prestupny);
+        
+        public static boolean isLeap(int datum) {
+            boolean prestupny = false;
+            if( (datum % 100 == 0) && (datum % 400 == 0) && (datum % 4 == 0) ){
+                prestupny = true;
+            }
+        return prestupny;
+        }
+        
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Zadej: dd mm rrrr");
+        int den = sc.nextInt();int mesic = sc.nextInt();int rok = sc.nextInt();
+        Calendar a = Calendar.getInstance(den, mesic, rok);
+        //System.out.println(a.dayInMonth());
+        do{
+        a.displayMonth();
+        System.out.println("");
+        System.out.println("Prikazy");
+        System.out.println("(+) pro pusunuti kalendare dopredu");
+        System.out.println("(-) pro posunuti dozadu");
+        System.out.println("(x) pro ukonceni");
+        String posun = sc.next();
+        //char znamenko = posun.CharAt(0);
+        String q = posun.substring(0);
+        
+        if(q.startsWith("+")){
+            a.nextMonth();
+        }
+        if(q.startsWith("-")){
+            a.previousMonth();
+        }
+        
+        if(q.startsWith("x")){
+            System.exit(0);
+        }
+        
+        }while(true);
+        //System.out.println(a.isLeap());
+    }
+        
     }
 }
