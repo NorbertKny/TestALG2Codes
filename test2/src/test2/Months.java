028efc1698013644eb480d3b3fe0738868120804
Norbert Roland Knýgit config --global user.email norbert.roland.kny@tul.czmkdir Codecd Codegit clone https://github.com/pepe/gitwstul22.gitgit config --global user.name Norbert Roland Knýgit config --global user.name <norbert.roland.kny@tul.cz>
2022-03-30 14:39:57 +0200

    Month+Enum

--- test2/src/test2/Months.java
+++ test2/src/test2/Months.java
@@ -9,5 +9,29 @@ package test2;
  * @author norbert.roland.kny
  */
 public enum Months {
+    JANUARY(1,31), FEBRUARY(2,28/29), DECEMBER(12,31);
     
+    private int monthNumber;
+    private int daysInMonth;
+    
+    private Months(int monthNumber, int daysInMonth){
+        this.monthNumber = monthNumber;
+        this.daysInMonth = daysInMonth;
+    }
+    
+    public int getMonthNumber(){
+        return monthNumber;
+    }
+    
+    public int getDaysInMont(){
+        return daysInMonth;
+    }
+    
+    public static void setLeapYear(){
+        Months.FEBRUARY.daysInMonth = 29;
+    }
+    
+    public static void setNormalYear(){
+        Months.FEBRUARY.daysInMonth = 28;
+    }
 }
