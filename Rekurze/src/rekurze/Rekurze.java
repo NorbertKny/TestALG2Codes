975f9b7f9594d6de0d13f488b653b4bdf5b33059
Norbert Roland Knýgit config --global user.email norbert.roland.kny@tul.czmkdir Codecd Codegit clone https://github.com/pepe/gitwstul22.gitgit config --global user.name Norbert Roland Knýgit config --global user.name <norbert.roland.kny@tul.cz>
2022-05-18 14:24:38 +0200

    newrek

--- Rekurze/src/rekurze/Rekurze.java
+++ Rekurze/src/rekurze/Rekurze.java
@@ -17,7 +17,7 @@ public class Rekurze {
         // TODO code application logic here
         
     }
-    public static int mystery(int a,int b){
+    public static int mystery(int a,int b){ //3,11
         if(b==0){
             return 0;
         }
@@ -26,5 +26,26 @@ public class Rekurze {
         }
         return mystery(a+a,b/2)+a;
     }
-    
+    /*
+    m(3,11) = m(6,5)+3 = 30+3 = 33
+    m(6,5)  = m(12,2)+6 = 24+6 = 30
+    m(12,2) = m(24,1) = 24
+    m(24,1) = m(48,0)+24 = 0+24 = 24
+    m(48,0) = 0
+    */
+    public static String mystery(int n){ //24
+        String s;
+        if(n<=1){
+            return String.valueOf(n);
+        }
+        s=mystery(n/2)+String.valueOf(n%2);
+        return s;
+    }
+    /*
+    m(24)   = m(12)+con0 ==>11000     final pro m(24)=11000
+    m(12)   = m(6)+con0  ==>1100
+    m(6)    = m(3)+con0  ==>110
+    m(3)    = m(1)+con1  ==>11
+    m(1)    = 1          ==>1
+    */
 }
